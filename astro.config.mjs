5a2de7a8b4c4aeecacafb2a2455541864a9fa675
takumimzd <retnuh.takumi@gmail.com>
2024-06-02 18:01:38 +0900

    rename repo & add prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,34 @@
 import astroRemark from '@astrojs/markdown-remark';
-import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig(
-	/** @type {import('astro').AstroUserConfig} */
-	{
-		markdown: {
-			render: [astroRemark, {
-				remarkPlugins: [],
-				rehypePlugins: ['rehype-slug', ['rehype-autolink-headings', {
-					behavior: 'wrap'
-				}]],
-				shikiConfig: {
-					theme: 'poimandres',
-					langs: [],
-					wrap: false
-				}
-			}]
-		},
-		site: 'http://localhost:3000/',
-		integrations: [sitemap()]
-	});
+  /** @type {import('astro').AstroUserConfig} */
+  {
+    markdown: {
+      render: [
+        astroRemark,
+        {
+          remarkPlugins: [],
+          rehypePlugins: [
+            'rehype-slug',
+            [
+              'rehype-autolink-headings',
+              {
+                behavior: 'wrap',
+              },
+            ],
+          ],
+          shikiConfig: {
+            theme: 'poimandres',
+            langs: [],
+            wrap: false,
+          },
+        },
+      ],
+    },
+    site: 'http://localhost:3000/',
+    integrations: [sitemap()],
+  },
+);
